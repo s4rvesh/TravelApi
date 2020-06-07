c09e5bf7551b26fa3f4d7eeef1f97b93313c3710
s4rvesh <sarvesh.upadhye@gmail.com>
2020-06-07 18:07:52 +0530

    update

---
+++ httpd/handler/nearBy.go
@@ -0,0 +1,58 @@
+package handler
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Cab struct {
+	vid      int    `json:"vid"`
+	vname    string `json:"vname"`
+	location int    `json:"location"`
+}
+
+func nearBy(c *gin.Context) {
+
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/travel")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Connected")
+
+	key := c.Param("location")
+	ki, _ := strconv.Atoi(key)
+	kb, kt := ki-3, ki+3
+
+	results, err := db.Query("SELECT vname, location, vid FROM cabs WHERE location BETWEEN ? AND ?", kb, kt)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for results.Next() {
+		var cab Cab
+
+		err = results.Scan(&cab.vname, &cab.location, &cab.vid)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		c.JSON(200, gin.H{
+			"vname":            cab.vname,
+			"location":         cab.location,
+			"vid":              cab.vid,
+			"current_Location": key,
+		})
+
+	}
+
+	defer results.Close()
+	defer db.Close()
+
+}
